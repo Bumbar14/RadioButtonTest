df2caf0dab460cc8914592f35ec31216803c4906
Bumbar14 <gregor.sabol@gmail.com>
2021-11-12 17:07:52 +0100

    Add project files.

---
+++ RadioButtonTest/RadioButtonTest/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FreshMvvm;
+
+namespace RadioButtonTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var page = FreshPageModelResolver.ResolvePageModel<FirstPageModel>();
+
+            var basicNavContainer = new FreshNavigationContainer(page);
+            MainPage = basicNavContainer;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RadioButtonTest/RadioButtonTest/Model/FirstPageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreshMvvm;
+using Xamarin.Forms;
+
+namespace RadioButtonTest
+{
+    class FirstPageModel : FreshBasePageModel
+    {
+
+        public Command GoToTestPage
+        {
+            get => new Command(async () =>
+            await CoreMethods.PushPageModel<SecondPageModel>());
+            
+        }
+    }
+}
